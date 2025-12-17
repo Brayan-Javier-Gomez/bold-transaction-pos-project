6c951852af87673f217020debd5df5783177bf0b
Bragom.ext <bragom.ext@eltiempo.com>
2025-12-16 19:03:41 -0500

    feat add transaction page layout

--- app/(transactions)/page.tsx
+++ app/(transactions)/page.tsx
@@ -1,7 +1,20 @@
+import styles from "./Transactions.module.scss";
+
+
 export default function Home() {
   return (
-    <div className="">
-      
-    </div>
+    <main className={styles.transaction__page}>
+      {/* Transaction resume */}
+      <section className={styles.transactions__resume}>
+
+      </section>
+
+      {/* Transaction table */}
+      <section className={styles.transactions__section}>
+        <div className={styles.transactions__section__header}>
+          <p className={styles.transactions__section__title}>Tus ventas de Diciembre</p>
+        </div>
+      </section>
+    </main>
   );
 }
