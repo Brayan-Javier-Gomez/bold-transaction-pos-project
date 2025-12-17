4a3c87c404fb1e486bcaf5241d262bafa653f408
Bragom.ext <bragom.ext@eltiempo.com>
2025-12-16 19:20:17 -0500

    feat add header shared component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.scss";
 import { montserrat } from "@/fonts/montserrat";
+import Header from "@/components/shared/Header";
 
 export default function RootLayout({
   children,
@@ -9,6 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.className} antialiased main-layout`}>
+        <Header />
         {children}
       </body>
     </html>
---
+++ components/shared/Header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import styles from "./Header.module.scss";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__content}>
+        <Image
+          className={styles.header__logo}
+          src="/assets/icons/bold-white.svg"
+          alt="Logo bold.co"
+          width={100}
+          height={100}
+        />
+        <nav className={styles.header__nav}>
+          <Link href="#" className={styles.header__nav__items}>
+            Mi negocio
+          </Link>
+          <Link href="#" className={styles.header__nav__items}>
+            Ayuda
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
