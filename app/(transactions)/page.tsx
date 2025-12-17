6d4995e7174c3ecd3c3a304da39686f07cb66341
Bragom.ext <bragom.ext@eltiempo.com>
2025-12-16 21:01:48 -0500

    feat add integrate components

--- app/(transactions)/page.tsx
+++ app/(transactions)/page.tsx
@@ -1,12 +1,16 @@
+import SalesResumeCard from "@/features/transactions/components/SalesResumeCard/SalesResumeCard";
 import styles from "./Transactions.module.scss";
-
+import PeriodSelector from "@/features/transactions/components/PeriodSelector/PeriodSelector";
+import Search from "@/features/transactions/components/Search/Search";
+import TransactionTable from "@/features/transactions/components/TransactionTable/TransactionTable";
 
 export default function Home() {
   return (
     <main className={styles.transaction__page}>
       {/* Transaction resume */}
       <section className={styles.transactions__resume}>
-
+        <SalesResumeCard />
+        <PeriodSelector />
       </section>
 
       {/* Transaction table */}
@@ -14,6 +18,8 @@ export default function Home() {
         <div className={styles.transactions__section__header}>
           <p className={styles.transactions__section__title}>Tus ventas de Diciembre</p>
         </div>
+        <Search />
+        <TransactionTable />
       </section>
     </main>
   );
