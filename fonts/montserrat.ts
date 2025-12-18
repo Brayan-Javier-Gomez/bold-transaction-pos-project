485b387dba55eb312f6408ec4a1601cdb6209746
Bragom.ext <bragom.ext@eltiempo.com>
2025-12-18 10:27:29 -0500

    fix font weight values

--- fonts/montserrat.ts
+++ fonts/montserrat.ts
@@ -3,4 +3,5 @@ import { Montserrat } from "next/font/google";
 export const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
+  weight: ["300", "400", "500", "600", "700","800"],
 });
